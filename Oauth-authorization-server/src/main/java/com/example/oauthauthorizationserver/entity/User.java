84e3c2583f8f4bad79ed05c3732940f09b78a0e4
igorp <igorprohorchenko@gmail.com>
2022-11-16 15:10:04 +0300

    oauth-authorization-server

---
+++ Oauth-authorization-server/src/main/java/com/example/oauthauthorizationserver/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.oauthauthorizationserver.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String firstName;
+    String lastName;
+    String email;
+
+    @Column(length = 60)
+    String password;
+
+    String role;
+    boolean enabled = false;
+}
